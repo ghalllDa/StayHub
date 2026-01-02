a9a68dc1430fe98df787173c2337378631f53c93
Ghalda Nur Zahrah <nurzahraghalda@gmail.com>
2026-01-02 21:12:34 +0700

    abc

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -1,8 +1,6 @@
 <?php
 
 return [
-    'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
-    'api_key'    => env('CLOUDINARY_API_KEY'),
-    'api_secret' => env('CLOUDINARY_API_SECRET'),
-    'secure'     => true,
+    'cloud_url' => env('CLOUDINARY_URL'),
+    'secure' => true,
 ];
