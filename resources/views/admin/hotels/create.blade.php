1584691223d8f941a192ff43f5cee3bbc1afe25e
Ghalda Nur Zahrah <nurzahraghalda@gmail.com>
2026-01-02 19:29:26 +0700

    admin

--- resources/views/admin/hotels/create.blade.php
+++ resources/views/admin/hotels/create.blade.php
@@ -4,7 +4,7 @@
 
             {{-- HEADER --}}
             <div class="mb-8">
-                <a href="{{ route('hotels.index') }}"
+                <a href="{{ route('admin.hotels.index') }}"
                    class="inline-flex items-center gap-2 text-sm text-gray-500 hover:text-gray-700 mb-4">
                     ← Kembali ke Daftar Hotel
                 </a>
