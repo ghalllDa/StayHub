2309e474bdd49bc57a852edf152d177b27941825
Ghalda Nur Zahrah <nurzahraghalda@gmail.com>
2026-01-04 22:19:27 +0700

    final

--- resources/views/dashboard/user.blade.php
+++ resources/views/dashboard/user.blade.php
@@ -222,7 +222,6 @@
 
             function renderHotels(data) {
                 list.innerHTML = '';
-                renderMarkers(data);
 
                 if (!data.length) {
                     list.innerHTML =
